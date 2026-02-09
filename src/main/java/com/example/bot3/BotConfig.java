a463a0a901a102132cd9e75e58bb9956830bf910
diyorbektolaganov299-hue <diyorbektolaganov299@gmail.com>
2026-02-09 16:56:05 +0500

    hubuhbuhb

--- src/main/java/com/example/bot3/BotConfig.java
+++ src/main/java/com/example/bot3/BotConfig.java
@@ -1,6 +1,13 @@
 package com.example.bot3;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 @Configuration
 public class BotConfig {
     @Autowired
